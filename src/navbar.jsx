da6eb5acfd3bef956dce49a98df8921cbaca648f
Aliyan15 <aliyanamir15@gmail.com>
2024-06-27 09:34:14 -0700

    changes

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -2,18 +2,25 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import "./navber.css";
+import {Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 function Menu() {
   return (
     <Navbar expand="lg" className="Navbar bg-white">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home"><img src="https://internee.pk/images/logo.webp" className='image'></img></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto items">
-            <Nav.Link href="#home">Internship</Nav.Link>
-            <Nav.Link href="#link">Company Collaboration</Nav.Link>
-            <Nav.Link href="#link">Contact Us</Nav.Link>
-          </Nav>
+          <div className='container'>
+            <Nav className="me-auto items">
+              <Nav.Link href="#home" className='items items1'>Internship</Nav.Link>
+              <Nav.Link href="#link" className='items items1'>Company Collaboration</Nav.Link>
+              <Nav.Link href="#link" className='items items1'>Contact Us</Nav.Link>
+              <Button className='btn btn-success button button1'>Job Portal</Button>
+            </Nav>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
