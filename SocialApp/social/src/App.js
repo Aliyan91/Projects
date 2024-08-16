2d3f9119f166212891c46117160feaecff4bd237
Aliyan15 <aliyanamir15@gmail.com>
2024-08-16 11:00:47 -0700

    Post

--- SocialApp/social/src/App.js
+++ SocialApp/social/src/App.js
@@ -1,7 +1,9 @@
 
 import './App.css';
+import Post from './components/CreatePost';
 import Futer from './components/Footer';
 import Header from './components/Header';
+import Card from './components/Post';
 import Sidebar from './components/Sidebar';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
      <Sidebar></Sidebar>
      <div className='content'>
         <Header></Header>
+        <Post></Post>
+        <Card></Card>
         <Futer></Futer>
      </div>
     </div>
---
+++ SocialApp/social/src/components/Post.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Card() {
+    return (
+        <div class="card" style={{width: "18rem"}}>
+            <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+        </div>
+    )
+}  
