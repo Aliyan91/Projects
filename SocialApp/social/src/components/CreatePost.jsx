c2a425b1bc6a08467de8bd386b15ab0e8c7f0aee
Aliyan15 <aliyanamir15@gmail.com>
2024-08-28 05:56:42 -0700

    Add and delete done

--- SocialApp/social/src/components/CreatePost.jsx
+++ SocialApp/social/src/components/CreatePost.jsx
@@ -1,22 +1,56 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useContext, useRef } from "react";
+import { Postlist } from "../store/post-list-store";
 
 export default function CreatePost() {
+    const id=useRef();
+    const title=useRef();
+    const body=useRef();
+    const reactions=useRef();
+    const tags=useRef();
+
+    const {addpost}=useContext(Postlist);
+
+    function handlesubmit(event){
+        event.preventDefault();
+        const userid=id.current.value;
+        const usertitle=title.current.value;
+        const userbody=body.current.value;
+        const userreactions=reactions.current.value;
+        const usertags=tags.current.value.split(" ");
+
+        addpost(userid,usertitle,userbody,userreactions,usertags);
+
+    }
     return (
-        <form>
+        <form className="create-post card p-2" onSubmit={handlesubmit}>
+
             <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                <label htmlFor="id" className="form-label">User ID</label>
+                <input type="text" className="form-control" id="id" placeholder="Enter user id" ref={id}/>
             </div>
+
             <div className="mb-3">
-                <label for="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1"/>
+                <label htmlFor="title" className="form-label">Post Title</label>
+                <input type="text" className="form-control" id="title" placeholder="How are you feeling today" ref={title}/>
             </div>
-            <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+
+            <div className="mb-3">
+                <label htmlFor="body" className="form-label">Content</label>
+                <textarea type="text" className="form-control" rows="4" id="body" placeholder="Tell us more" ref={body}/>
             </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
+
+            <div className="mb-3">
+                <label htmlFor="reactions" className="form-label">Reactions</label>
+                <input type="text" className="form-control" id="reactions" placeholder="How many people reacted" ref={reactions}/>
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="tags" className="form-label">Tags</label>
+                <input type="text" className="form-control" id="tags" placeholder="Enter hashtags" ref={tags}/>
+            </div>
+            
+            <button type="submit" className="btn btn-primary" style={{width:"100px"}}>Post</button>
         </form>
     );
 }
--- SocialApp/social/src/store/post-list-store.jsx
+++ SocialApp/social/src/store/post-list-store.jsx
@@ -1,23 +1,46 @@
 import { createContext, useReducer } from "react";
 
-export const Postlist=createContext({
-    postList:[],
-    addpost:()=>{},
-    deletepost:()=>{}
+export const Postlist = createContext({
+    postList: [],
+    addpost: () => { },
+    deletepost: () => { }
 });
 
-function postlistReducer(currPostList,action){
-    return currPostList;
+function postlistReducer(currPostList, action) {
+    let newpostlist = currPostList;
+    if (action.type === "Delete_Post") {
+        newpostlist = currPostList.filter((post) => post.id !== action.payload.id);
+    }
+    else if(action.type === "Add_Post"){
+        newpostlist=[action.payload,...currPostList];
+    }
+    return newpostlist;
 }
-const PostListProvider=({children})=>{
-    const [postList,dispatchpostlist]= useReducer(postlistReducer,Default_POST_LIST);
+const PostListProvider = ({ children }) => {
+    const [postList, dispatchpostlist] = useReducer(postlistReducer, Default_POST_LIST);
 
-    function addpost(){
-
+    function addpost(userid, usertitle, userbody, userreactions, usertags) {
+        dispatchpostlist({
+            type: "Add_Post",
+            payload: {
+                id: Date.now() ,
+                title: usertitle,
+                body: userbody,
+                reactions: userreactions,
+                userId: userid,
+                tags: usertags,
+            }
+        })
     }
 
-    function deletepost(id){
-        console.log(id);
+    function deletepost(id) {
+        dispatchpostlist({
+            type: "Delete_Post",
+            payload: {
+                id,
+            },
+        })
+
     }
     return <Postlist.Provider value={{
         postList,
@@ -27,22 +50,22 @@ const PostListProvider=({children})=>{
     }>{children}</Postlist.Provider>
 }
 
-const Default_POST_LIST=[
+const Default_POST_LIST = [
     {
-        id:"1",
-        title:"Going to Lahore",
-        body:"Hi friends I am going to Lahore.Hope to enjoy alot.",
-        reactions:2,
-        userId:"user-9",
-        tags:["vacations","Lahore","Enjoying"],
+        id: "1",
+        title: "Going to Lahore",
+        body: "Hi friends I am going to Lahore.Hope to enjoy alot.",
+        reactions: 2,
+        userId: "user-9",
+        tags: ["vacations", "Lahore", "Enjoying"],
     },
     {
-        id:"2",
-        title:"Pass ho gaye",
-        body:"4 saal ke masti ka bad bhi ho gaye pass.",
-        reactions:15,
-        userId:"user-12",
-        tags:["Graduated","Unbelieveable"],
+        id: "2",
+        title: "Pass ho gaye",
+        body: "4 saal ke masti ka bad bhi ho gaye pass.",
+        reactions: 15,
+        userId: "user-12",
+        tags: ["Graduated", "Unbelieveable"],
     },
 
 ];
