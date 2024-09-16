6094ffaa4c4e2accff3294e3f1486838c4651209
Aliyan15 <aliyanamir15@gmail.com>
2024-09-16 05:07:09 -0700

    new

--- SocialApp/social/src/components/Postlist.jsx
+++ SocialApp/social/src/components/Postlist.jsx
@@ -1,22 +1,31 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "./Post";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Postlist } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import Loadingspinner from "./Loadingspinner";
 
 export default function Cardlist() {
     const { postList,addinitialposts } = useContext(Postlist);
 
+    const[fetching,setfetching]=useState(false);
     useEffect(()=>{
+        
+        setfetching(true);
         fetch('https://dummyjson.com/posts?select=title,body,tags,userId')
         .then(res => res.json())
-        .then((data)=>{addinitialposts(data.posts)});
+        .then((data)=>{
+            addinitialposts(data.posts)
+            setfetching(false);
+        });
+
     },[]);
 
     return (
         <>
-            {postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
-            {postList.map((post) => (
+            {fetching && <Loadingspinner></Loadingspinner>}
+            {!fetching && postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
+            {!fetching && postList.map((post) => (
                 <Card key={post.id} post={post}/>
             ))}
         </>
