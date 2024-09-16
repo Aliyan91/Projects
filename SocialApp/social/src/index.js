d95f41c1c9d05797b46481113fcfc7cafd2af386
Aliyan15 <aliyanamir15@gmail.com>
2024-09-16 05:56:10 -0700

    gg

--- SocialApp/social/src/index.js
+++ SocialApp/social/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {RouterProvider, createBrowserRouter } from "react-router-dom";
+import CreatePost from './components/CreatePost';
+
+const router =createBrowserRouter([
+  {path: "/",element:<App/>},
+  {path: "/",element:<App/>},
+  {path: "/create-post",element:<CreatePost/>}
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
